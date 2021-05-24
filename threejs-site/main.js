ba356f6e4cfde4aaa00ba3587e825f7d53a37a38
migben <itsmigben@gmail.com>
2021-05-24 19:43:41 -0400

    adjusted the position of the moon

--- threejs-site/main.js
+++ threejs-site/main.js
@@ -4,6 +4,8 @@ import * as THREE from 'three'
 
 import { OrbitControls }  from 'three/examples/jsm/controls/OrbitControls'
 
+// Quick Setup
+
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -19,11 +21,15 @@ camera.position.setZ(30)
 
 renderer.render(scene, camera)
 
+// Torus 3D object renderer
+
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
 const material = new THREE.MeshStandardMaterial( { color: 0x39a9db } )
 const torus = new THREE.Mesh( geometry, material )
 
 scene.add(torus)
+
+// Lights - point specific and ambient light
 
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(20, 20, 20)
@@ -31,11 +37,13 @@ pointLight.position.set(20, 20, 20)
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight, ambientLight)
 
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50)
-scene.add( lightHelper, gridHelper )
+// Helpers
 
-const controls = new OrbitControls(camera, renderer.domElement)
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50)
+// scene.add( lightHelper, gridHelper )
+
+// const controls = new OrbitControls(camera, renderer.domElement)
 
 // Populating the outer space with 3D objects
 
@@ -52,13 +60,43 @@ function addStar(){
 
 Array(200).fill().forEach(addStar)
 
+// Background
 
 const spaceTexture = new THREE.TextureLoader().load('stars2.jpg')
 scene.background = spaceTexture
 
 // Avatar
+const benTexture = new THREE.TextureLoader().load('hero-img.png')
 
+const ben= new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial( { map: benTexture } )
+)
 
+scene.add(ben)
+
+// Moon
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg')
+const normalTexture = new THREE.TextureLoader().load('normal.jpg')
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture
+  })
+)
+
+scene.add(moon)
+
+moon.position.z = 30
+moon.position.setX(-10)
+
+jeff.position.z = -5
+jeff.position.x = 2
+
+// scroll animation
 
 
 
