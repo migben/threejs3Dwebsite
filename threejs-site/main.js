a5ed88263a775c0e415c7cff04b8b1ade52c7b78
migben <itsmigben@gmail.com>
2021-05-24 16:59:47 -0400

    finshed applying lighting, grid and movement animation

--- threejs-site/main.js
+++ threejs-site/main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three'
 
+import { OrbitControls }  from 'three/examples/jsm/controls/OrbitControls'
+
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -17,4 +19,42 @@ camera.position.setZ(30)
 
 renderer.render(scene, camera)
 
-const geometry;
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshStandardMaterial( { color: 0xff6347 } )
+const torus = new THREE.Mesh( geometry, material )
+
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(20, 20, 20)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add( lightHelper, gridHelper )
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+// Populating the outer space with 3D objects
+
+
+
+
+
+
+
+
+function animate(){
+  requestAnimationFrame( animate )
+
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.005
+
+  controls.update()
+
+  renderer.render( scene, camera )
+}
+
+animate()
