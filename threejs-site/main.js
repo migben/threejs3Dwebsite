35c31b37572f94316163837cf0c979c86aa137bf
migben <itsmigben@gmail.com>
2021-05-24 17:20:57 -0400

    added stars and spacetexture wallpaper

--- threejs-site/main.js
+++ threejs-site/main.js
@@ -20,7 +20,7 @@ camera.position.setZ(30)
 renderer.render(scene, camera)
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshStandardMaterial( { color: 0xff6347 } )
+const material = new THREE.MeshStandardMaterial( { color: 0x39a9db } )
 const torus = new THREE.Mesh( geometry, material )
 
 scene.add(torus)
@@ -39,7 +39,24 @@ const controls = new OrbitControls(camera, renderer.domElement)
 
 // Populating the outer space with 3D objects
 
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } )
+  const star = new THREE.Mesh( geometry, material )
 
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+
+  star.position.set(x,y,z)
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+
+const spaceTexture = new THREE.TextureLoader().load('stars2.jpg')
+scene.background = spaceTexture
+
+// Avatar
 
 
 
